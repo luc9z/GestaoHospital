dd25ecb1a824548a5b759755ea98f94385ddb2d1
Lucas <131416070+luc9z@users.noreply.github.com>
2024-09-05 08:52:56 -0300

    Update DatabaseConnection.java

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/GestaoHospital";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "123"; // Adicione a senha aqui
+    private static final String PASSWORD = "123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
